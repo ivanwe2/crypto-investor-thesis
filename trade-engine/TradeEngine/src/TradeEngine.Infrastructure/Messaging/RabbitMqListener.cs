a584eb99bf28ea07d52406df1c30bcd841175c08
Ivan Rusev <ivanrusev03@schoolmath.eu>
2026-02-16 01:01:00 +0200

    fix: add import

--- trade-engine/TradeEngine/src/TradeEngine.Infrastructure/Messaging/RabbitMqListener.cs
+++ trade-engine/TradeEngine/src/TradeEngine.Infrastructure/Messaging/RabbitMqListener.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
