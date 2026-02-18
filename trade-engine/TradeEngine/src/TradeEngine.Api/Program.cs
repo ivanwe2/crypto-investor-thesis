4a5a39ba62b30efba026f777429e861cfcb40302
Ivan Rusev <Ivan.Rusev@indeavr.com>
2026-02-18 18:13:03 +0200

    feat: add jwt auth

--- trade-engine/TradeEngine/src/TradeEngine.Api/Program.cs
+++ trade-engine/TradeEngine/src/TradeEngine.Api/Program.cs
@@ -1,56 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using TradeEngine.Api.Endpoints;
+using TradeEngine.Api.Extensions;
 using TradeEngine.Api.Middleware.ExceptionHandling;
 using TradeEngine.Application.Constants;
 using TradeEngine.Application.Interfaces;
+using TradeEngine.Infrastructure.Extensions;
 using TradeEngine.Infrastructure.Messaging;
 using TradeEngine.Infrastructure.Persistence;
 using TradeEngine.Infrastructure.Services;
 using TradeEngine.Infrastructure.SignalR.Hubs;
 using TradeEngine.Infrastructure.SignalR.Services;
-using TradeEngine.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration));
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<TradeEngineDbContext>(options =>
-{
-    options.UseNpgsql(connectionString);
-});
-
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddProblemDetails();
-
-builder.Services.AddSignalR();
-
-builder.Services.AddSingleton<IPriceBroadcaster, PriceBroadcaster>();
-builder.Services.AddHttpClient<IAiAnalyst, HttpAiAnalyst>(client =>
-{
-    string aiUrl = builder.Configuration[MessagingConstants.AiAnalystUrlConfigKey] 
-                   ?? MessagingConstants.DefaultAiUrl;
-                   
-    client.BaseAddress = new Uri(aiUrl);
-});
-
-builder.Services.AddHostedService<RabbitMqListener>();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("ReactClient", policy =>
-    {
-        policy.WithOrigins("http://localhost:3000")
-              .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
-    });
-});
+builder.Services.AddPresentationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddSecurityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -64,7 +33,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseSerilogRequestLogging();
 app.UseCors("ReactClient");
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapHub<MarketDataHub>("/hubs/market");
 app.MapAnalysisEndpoints();
---
+++ trade-engine/TradeEngine/src/TradeEngine.Application/Constants/JwtConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace TradeEngine.Application.Constants;
+
+public static class JwtConstants
+{
+    public const string ConfigKey = "Jwt:Secret";
+    public const string Issuer = "trade-engine";
+    public const string Audience = "web-client";
+    public const string DefaultSecret = "super_secret_key_for_thesis_development_only_123!";
+}
